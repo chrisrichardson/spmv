07d3349a4179a4a31b74531586a74d46de82a027
Chris Richardson <chris@bpi.cam.ac.uk>
2020-02-10 09:04:06 +0000

    Make A in a function

--- main.cpp
+++ main.cpp
@@ -18,6 +18,73 @@
 #include "DistributedVector.h"
 
 //-----------------------------------------------------------------------------
+// Divide size into N ~equal chunks
+std::vector<index_type> owner_ranges(std::int64_t size, index_type N)
+{
+  // Compute number of items per process and remainder
+  const std::int64_t n = N / size;
+  const std::int64_t r = N % size;
+
+  // Compute local range
+  std::vector<index_type> ranges;
+  for (int rank = 0; rank < (size + 1); ++rank)
+  {
+    if (rank < r)
+      ranges.push_back(rank * (n + 1));
+    else
+      ranges.push_back(rank * n + r);
+  }
+
+  return ranges;
+}
+//-----------------------------------------------------------------------------
+Eigen::SparseMatrix<double, Eigen::RowMajor> create_A(MPI_Comm comm)
+{
+  int mpi_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  int mpi_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+  // Make a square Matrix divided evenly across cores
+  int N = 5000;
+
+  std::vector<index_type> ranges = owner_ranges(mpi_size, N);
+
+  index_type r0 = ranges[mpi_rank];
+  index_type r1 = ranges[mpi_rank + 1];
+  int M = r1 - r0;
+
+  std::cout << "# " << r0 << "-" << r1 << " \n";
+
+  // Local part of the matrix
+  // Must be RowMajor and compressed
+  Eigen::SparseMatrix<double, Eigen::RowMajor> A(M, N);
+
+  // Set up A
+  // Add entries on all local rows
+  // Using [local_row, global_column] indexing
+  double gamma = 0.1;
+  for (int i = 0; i < M; ++i)
+  {
+    // Global column diagonal index
+    int c0 = r0 + i;
+    // Special case for very first and last global rows
+    if (c0 == 0)
+      A.insert(i, c0) = 1.0;
+    else if (c0 == (N - 1))
+      A.insert(i, c0) = 1.0;
+    else
+    {
+      A.insert(i, c0 - 1) = gamma;
+      A.insert(i, c0) = 1.0 - 2.0 * gamma;
+      A.insert(i, c0 + 1) = gamma;
+    }
+  }
+  A.makeCompressed();
+
+  return A;
+}
+//-----------------------------------------------------------------------------
 // Untested CG solver
 void cg(const Eigen::Ref<const Eigen::SparseMatrix<double, Eigen::RowMajor>>& A,
         const Eigen::Ref<const Eigen::VectorXd>& b)
@@ -68,73 +135,20 @@ void cg(const Eigen::Ref<const Eigen::SparseMatrix<double, Eigen::RowMajor>>& A,
   }
 }
 //-----------------------------------------------------------------------------
-// Divide size into N ~equal chunks
-std::vector<index_type> owner_ranges(std::int64_t size, index_type N)
-{
-  // Compute number of items per process and remainder
-  const std::int64_t n = N / size;
-  const std::int64_t r = N % size;
-
-  // Compute local range
-  std::vector<index_type> ranges;
-  for (int rank = 0; rank < (size + 1); ++rank)
-  {
-    if (rank < r)
-      ranges.push_back(rank * (n + 1));
-    else
-      ranges.push_back(rank * n + r);
-  }
-
-  return ranges;
-}
-//-----------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
   MPI_Init(&argc, &argv);
 
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int mpi_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  int mpi_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
-  std::cout << "# rank = " << rank << "/" << size << "\n";
+  Eigen::SparseMatrix<double, Eigen::RowMajor> A = create_A(MPI_COMM_WORLD);
+  int M = A.rows();
+  int N = A.cols();
 
-  // Make a square Matrix divided evenly across cores
-  int N = 5000;
-
-  std::vector<index_type> ranges = owner_ranges(size, N);
-
-  index_type r0 = ranges[rank];
-  index_type r1 = ranges[rank + 1];
-  int M = r1 - r0;
-
-  std::cout << "# " << r0 << "-" << r1 << " \n";
-
-  // Local part of the matrix
-  // Must be RowMajor and compressed
-  Eigen::SparseMatrix<double, Eigen::RowMajor> A(M, N);
-
-  // Set up A
-  // Add entries on all local rows
-  // Using [local_row, global_column] indexing
-  double gamma = 0.1;
-  for (int i = 0; i < M; ++i)
-  {
-    // Global column diagonal index
-    int c0 = r0 + i;
-    // Special case for very first and last global rows
-    if (c0 == 0)
-      A.insert(i, c0) = 1.0;
-    else if (c0 == (N - 1))
-      A.insert(i, c0) = 1.0;
-    else
-    {
-      A.insert(i, c0 - 1) = gamma;
-      A.insert(i, c0) = 1.0 - 2.0 * gamma;
-      A.insert(i, c0 + 1) = gamma;
-    }
-  }
-  A.makeCompressed();
+  std::cout << "# rank = " << mpi_rank << "/" << mpi_size << "\n";
 
 #ifdef HAS_MKL
   // Remap columns to local indexing for MKL
@@ -209,25 +223,11 @@ int main(int argc, char** argv)
   double pnorm_sum;
   MPI_Allreduce(&pnorm, &pnorm_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-  if (rank == 0)
+  if (mpi_rank == 0)
   {
     std::cout << "time = " << diff.count() << "s.\n";
     std::cout << "norm = " << pnorm_sum << "\n";
   }
-
-  // // Output
-  // std::stringstream s;
-  // s << rank << " [";
-  // for (int i = 0; i < M; ++i)
-  //   s << p[i] << " ";
-  // s << "]";
-
-  // for (int i = 0; i < size; ++i)
-  // {
-  //   if (i == rank)
-  //     std::cout << s.str() << "\n";
-  //   MPI_Barrier(MPI_COMM_WORLD);
-  // }
 
   // Destroy here before MPI_Finalize, because it holds a comm
   psp.reset();
