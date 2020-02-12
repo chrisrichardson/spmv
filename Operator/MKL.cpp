aae17c98e046073579498c9b69e37f6c4d3a1f72
Jeffrey Salmond <js947@cam.ac.uk>
2020-02-12 21:37:01 +0000

    move MKL operator into its own file

---
+++ Operator/MKL.cpp
@@ -0,0 +1,23 @@
+#include "MKL.h"
+
+#define sparse_CHECK(x)if(x != SPARSE_STATUS_SUCCESS) throw std::runtime_error(#x " failed")
+
+OperatorMKL::OperatorMKL(Eigen::SparseMatrix<double, Eigen::RowMajor>& A)
+{
+  sparse_CHECK(mkl_sparse_d_create_csr(
+      &A_mkl, SPARSE_INDEX_BASE_ZERO, A.rows(), A.cols(), A.outerIndexPtr(),
+      A.outerIndexPtr() + 1, A.innerIndexPtr(), A.valuePtr()));
+
+  sparse_CHECK(mkl_sparse_optimize(A_mkl));
+
+  mat_desc.type = SPARSE_MATRIX_TYPE_GENERAL;
+  mat_desc.diag = SPARSE_DIAG_NON_UNIT;
+}
+
+Eigen::VectorXd OperatorMKL::apply(Eigen::VectorXd& psp) const
+{
+  Eigen::VectorXd q(psp.size());
+  mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0, A_mkl, mat_desc,
+                  psp.data(), 0.0, q.data());
+  return q;
+}
--- Operator/MKL.h
+++ Operator/MKL.h
@@ -7,26 +7,6 @@ class OperatorMKL : public Operator {
   struct matrix_descr mat_desc;
 
 public:
-  OperatorMKL(Eigen::SparseMatrix<double, Eigen::RowMajor>& A) {
-    sparse_status_t status = mkl_sparse_d_create_csr(
-        &A_mkl, SPARSE_INDEX_BASE_ZERO, A.rows(), A.cols(), A.outerIndexPtr(),
-        A.outerIndexPtr() + 1, A.innerIndexPtr(), A.valuePtr());
-    assert(status == SPARSE_STATUS_SUCCESS);
-
-    status = mkl_sparse_optimize(A_mkl);
-    assert(status == SPARSE_STATUS_SUCCESS);
-
-    if (status != SPARSE_STATUS_SUCCESS)
-        throw std::runtime_error("Could not create MKL matrix");
-
-    mat_desc.type = SPARSE_MATRIX_TYPE_GENERAL;
-    mat_desc.diag = SPARSE_DIAG_NON_UNIT;
-   }
-
-   Eigen::VectorXd apply(Eigen::VectorXd &psp) const {
-    Eigen::VectorXd q(psp.size());
-    mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0, A_mkl, mat_desc,
-                    psp.data(), 0.0, q.data());
-    return q;
-   }
+  OperatorMKL(Eigen::SparseMatrix<double, Eigen::RowMajor>& A);
+  Eigen::VectorXd apply(Eigen::VectorXd &psp) const;
 };
