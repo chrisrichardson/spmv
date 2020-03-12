e8d6773943fe3607fcb5e3c9d343626472ce89c7
Chris Richardson <chris@bpi.cam.ac.uk>
2020-03-12 14:20:45 +0000

    Add spmv.h

---
+++ spmv.h
@@ -0,0 +1,7 @@
+// Copyright (C) 2020 Chris Richardson (chris@bpi.cam.ac.uk) and Jeffrey Salmond
+// SPDX-License-Identifier:    MIT
+
+#include "L2GMap.h"
+#include "Matrix.h"
+#include "read_petsc.h"
+#include "cg.h"
